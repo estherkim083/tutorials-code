3ae87a9eff3f88e6fe078c5daa045b9da7761b31
estherkim083 <estherkim083@gmail.com>
2022-11-11 15:50:47 +0900

    init

---
+++ blog_api/urls.py
@@ -0,0 +1,21 @@
+from .views import PostList, PostDetail, PostSearch, PostListDetailfilter, CreatePost, EditPost, AdminPostDetail, DeletePost
+#from rest_framework.routers import DefaultRouter
+from django.urls import path
+
+app_name = 'blog_api'
+
+# router = DefaultRouter()
+# router.register('', PostList, basename='post')
+# urlpatterns = router.urls
+
+urlpatterns = [
+    # path('posts/<str:pk>/', PostDetail.as_view(), name='detailcreate'),
+    path('posts/', PostDetail.as_view(), name='detailcreate'),
+    path('search/', PostListDetailfilter.as_view(), name='postsearch'),
+    path('', PostList.as_view(), name='listcreate'),
+    # Post Admin URLs
+    path('admin/create/', CreatePost.as_view(), name='createpost'),
+    path('admin/edit/postdetail/<int:pk>/', AdminPostDetail.as_view(), name='admindetailpost'),
+    path('admin/edit/<int:pk>/', EditPost.as_view(), name='editpost'),
+    path('admin/delete/<int:pk>/', DeletePost.as_view(), name='deletepost'),
+]
