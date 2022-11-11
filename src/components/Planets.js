898e6d5eecf06bca45cc23dc89abfd439888cf67
estherkim083 <estherkim083@gmail.com>
2022-11-11 15:44:30 +0900

    init

---
+++ src/components/Planets.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import { useQuery } from 'react-query';
+import Planet from './Planet';
+
+const fetchPlanets= async ({queryKey}) => {
+    const [key, page]= queryKey;
+    const res= await fetch(`https://swapi.dev/api/planets/?page=${page}`);
+    return res.json();
+}
+
+const Planets = () => {
+    const [ page, setPage ]= useState(1);
+    const { data, status } = useQuery(['planets', page], fetchPlanets);
+    console.log(data);
+
+    return ( 
+        <div>
+            <h2>Planets</h2>
+            <button onClick={()=> setPage(1)}>page 1</button>
+            <button onClick={()=> setPage(2)}>page 2</button>
+            <button onClick={()=> setPage(3)}>page 3</button>
+            { status === 'loading' && (
+                <div>Loading data...</div>
+            )}
+            { status === 'error' && (
+                <div>Error fetching data</div>
+            )}
+            { status === 'success' && (
+                <div>
+                    { data.results.map(planet => 
+                    <Planet key= {planet.name} planet= {planet}/>)}
+                </div>
+            )}
+        </div>
+    );
+}
+ 
+export default Planets;
